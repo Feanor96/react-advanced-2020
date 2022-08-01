f71dfae0e4d99936a028e6bd0008ffd10c09a831
Salim <78372105+Feanor96@users.noreply.github.com>
2022-08-02 01:06:18 +0530

    use state basics

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Setup from './tutorial/1-useState/setup/1-error-example'
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced React Tutorial</h2>
+      <Setup />
     </div>
   )
 }
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
+  const [text, setText] = useState()
   return <h2>useState basic example</h2>;
 };
 
